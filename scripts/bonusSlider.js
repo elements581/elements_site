930b7ac0ec5038d239029e670feb66f52d3268a1
elements581 <andriydevlord@gmail.com>
2025-04-17 23:19:36 +0300

    Update bonusSlider.js update slider add swipe

--- scripts/bonusSlider.js
+++ scripts/bonusSlider.js
@@ -3,15 +3,21 @@ const cards = document.querySelectorAll('.bonus-slider-card');
 const dots = document.querySelectorAll('.bonus-slider-dot');
 
 function getTransformValue(offset) {
-    
     const windowWidth = window.innerWidth;
 
-    if (windowWidth < 480) {
-       
-        return `${offset * 25}%`;
+    if (windowWidth < 358) {
+        return `${offset * 30}%`;
+    } else if (windowWidth < 370) {
+        return `${offset * 33}%`;
+    } else if (windowWidth < 390) {
+        return `${offset * 35}%`;
+    } else if (windowWidth < 410) {
+        return `${offset * 40}%`;
+    } else if (windowWidth < 480) {
+        return `${offset * 45}%`;
+    } else {
+        return `${offset * 90}%`;
     }
-    
-    return `${offset * 90}%`;
 }
 
 function updateBonusCards() {
@@ -21,16 +27,12 @@ function updateBonusCards() {
             card.style.zIndex = '2'; 
             card.style.top = '0'; 
             card.classList.add('active'); 
-            
         } else if (index === (currentIndex + 1) % cards.length) {
-            
             card.style.transform = `translateX(${getTransformValue(-1)})`; 
             card.style.zIndex = '1'; 
             card.style.top = '35px'; 
             card.classList.remove('active'); 
-            
         } else {
-           
             card.style.transform = `translateX(${getTransformValue(1)})`; 
             card.style.zIndex = '1'; 
             card.style.top = '35px'; 
@@ -42,6 +44,9 @@ function updateBonusCards() {
         dot.classList.toggle('active', index === currentIndex);
     });
 }
+
+let startX = 0;
+let endX = 0;
 
 dots.forEach((dot, index) => {
     dot.addEventListener('click', () => {
@@ -61,4 +66,24 @@ document.querySelector('.btn-prev').addEventListener('click', () => {
 });
 
 
-updateBonusCards();
+const sliderContainer = document.querySelector('.bonus-slider-container'); 
+sliderContainer.addEventListener('touchstart', (e) => {
+    startX = e.touches[0].clientX; 
+});
+
+sliderContainer.addEventListener('touchmove', (e) => {
+    endX = e.touches[0].clientX; 
+});
+
+sliderContainer.addEventListener('touchend', () => {
+    const threshold = 50; 
+    if (startX - endX > threshold) {
+        currentIndex = (currentIndex + 1) % cards.length;
+        updateBonusCards();
+    } else if (endX - startX > threshold) {
+        currentIndex = (currentIndex - 1 + cards.length) % cards.length;
+        updateBonusCards();
+    }
+});
+
+window.addEventListener("resize", updateBonusCards);
