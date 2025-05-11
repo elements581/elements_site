40b27a7e6418f1ab0c4fa04039ebde019ca1c7dd
elements581 <andriydevlord@gmail.com>
2025-05-11 22:19:51 +0300

    Update main.js update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,8 @@ import { applyBlurOnScroll } from "./blurOnScroll.js";
 import { initVideoModal } from "./videoBisCard.js";
 import { initVideoReview } from "./videoReviews.js"
 import { headerDropdownOpen } from "./openHeaderDropdown.js";
+import { initFormValidation } from "./validationForm.js";
+import { initMobileMenu } from "./mobileBurgerMenu.js";
 import initializeReviewsSlider  from './reviewsSlider.js';
 import initializeFormToggle from './formToggle.js';
 
@@ -15,9 +17,8 @@ initVideoReview();
 headerDropdownOpen();
 
 document.addEventListener('DOMContentLoaded', () => {
+    initMobileMenu();
     initializeReviewsSlider();
-});
-
-document.addEventListener('DOMContentLoaded', () => {
     initializeFormToggle();
+    initFormValidation();
 });
